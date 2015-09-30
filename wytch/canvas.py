7ed33fcff61a90a48b1babee75ff3ccc308b528a
Josef Gajdusek <atx@atx.name>
2015-09-30 16:03:45 +0200

    Canvas: Draw .square() by lines

--- wytch/canvas.py
+++ wytch/canvas.py
@@ -70,8 +70,8 @@ class Canvas:
         pass
 
     def square(self, x, y, width, height, bordercolor = colors.WHITE):
-        for xi in range(x, x + width):
-            for yi in range(y, y + height):
+        for yi in range(y, y + height):
+            for xi in range(x, x + width):
                 self.set(xi, yi, " ", fg = bordercolor, bg = bordercolor)
 
     def hline(self, x, y, length, fg = colors.WHITE, bg = colors.BLACK,
