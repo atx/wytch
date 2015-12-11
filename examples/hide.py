e2cd569ed7f07faef0caa544d25be288624b6d42
Josef Gajdusek <atx@atx.name>
2015-12-11 13:05:15 +0100

    examples/hide.py: Add

---
+++ examples/hide.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+#
+# Copyright (c) 2015 Josef Gajdusek
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from wytch import builder, colors, view, Wytch
+
+w = Wytch(debug = False)
+
+with w:
+    ttgt = view.TextInput()
+    lbl = view.Label("Target", fg = colors.BLUE)
+    with builder.Builder(w.root) as b:
+        def onchange(w, v):
+            ttgt.display = not ttgt.display
+            lbl.display = not lbl.display
+
+        b.align().box("Login").grid(3, 4) \
+            (lbl).spacer(width = 2)(ttgt) \
+            (view.Checkbox("Hide", onchange = onchange), colspan = 3)()() \
+            (view.Button("Quit", onpress = lambda _: w.exit()), colspan = 3)
